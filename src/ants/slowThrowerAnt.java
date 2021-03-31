2e9c708f182815e85ec0f55dcffc0e5d4a5e5d1b
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-03-31 21:28:05 +0100

    I am starting it now

--- src/ants/slowThrowerAnt.java
+++ src/ants/slowThrowerAnt.java
@@ -1,6 +1,23 @@
 package ants;
 
-public class slowThrowerAnt {
+import core.Ant;
+import core.AntColony;
+import core.Bee;
+
+public class slowThrowerAnt extends Ant {
+    /**
+     * Creates a new Ant, with a food cost of 0.
+     *
+     * @param armor The armor of the ant.
+     */
+    public slowThrowerAnt(int armor) {
+        super(armor);
+    }
+
+    //@java.lang.Override
+    public void action(AntColony colony) {
+
+    }
 
     //Starting work on it
 }
